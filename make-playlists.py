241576f3c5616fb8b9a09f7fba025204f4eb80e2
Stephanie K. Ananth <sananth@andrew.cmu.edu>
2017-04-11 01:05:01 -0400

    Update

--- make-playlists.py
+++ make-playlists.py
@@ -1,1 +1,163 @@
+# Stephanie K. Ananth (sananth)
 
+# Adapted from Paul Lamere's examples
+# https://github.com/plamere/spotipy
+
+import os
+import spotipy
+import spotipy.util as util
+import pprint
+
+# Client Credentials from Spotify Developer
+os.environ['SPOTIPY_CLIENT_ID'] = '5c14698fcf8e41adb4f39b5518e55100'
+os.environ['SPOTIPY_CLIENT_SECRET'] = '355d085eaf6049a1bda2e8dff93bdefd'
+os.environ['SPOTIPY_REDIRECT_URI'] = 'https://www.cs.cmu.edu/~112/index.html'
+
+# Takes a username and term (short_term, medium_term, long_term) in strings
+# Returns a list of the user's top songs' URIs of number of songs in an int
+def list_top_songs_by_term(username, term, n=100):
+    result = []
+    # Scope is the level of authentification to execute the desired function
+    scope = 'user-top-read'
+    token = util.prompt_for_user_token(username, scope)
+    sp = spotipy.Spotify(auth=token)
+    sp.trace = False
+    results = sp.current_user_top_tracks(time_range=term, limit=n)
+    for item in results['items']:
+        result += [item['uri']]
+    return result
+
+print(list_top_songs_by_term('shadykoopa', 'short-term', 10))
+
+# Takes a username and term (short_term, medium_term, long_term) in strings
+# Returns a list of the user's top artists' URIs of n artists in an integer
+def list_top_artists_by_term(username, term, n=10):
+    result = []
+    scope = 'user-top-read'
+    token = util.prompt_for_user_token(username, scope)
+    sp = spotipy.Spotify(auth=token)
+    sp.trace = False
+    results = sp.current_user_top_artists(time_range=term, limit=n)
+    for item in results['items']:
+        result += [item['uri']]
+    return result
+
+# Takes an artist URI in a string
+# Returns a list of their n (up to 10) top songs' URIs
+def list_top_songs_by_artist(artist_uri, n=10):
+    result = []
+    sp = spotipy.Spotify()
+    results = sp.artist_top_tracks(artist_uri)
+    for track in results['tracks']:
+        result += [track['uri']]
+    return result[:n]
+
+# Takes a username in a string and a name for the new playlist in a string
+# Creates empty Spotify playlist for the user in the playlist name
+# Returns the playlist's URI in a string
+def create_playlist(username, playlist_name):
+    scope = 'user-library-modify'
+    token = util.prompt_for_user_token(username, scope)
+    sp = spotipy.Spotify(auth=token)
+    sp.trace = False
+    playlist = sp.user_playlist_create(username, playlist_name)
+    return playlist['uri']
+
+# Takes a username and a playlist URI in a string and song URIs in a list
+# Adds the song to the user's playlist, returns None
+def add_songs_to_playlist(username, playlist_uri, song_uris):
+    scope = 'playlist-modify-public'
+    token = util.prompt_for_user_token(username, scope)
+    sp = spotipy.Spotify(auth=token)
+    sp.trace = False
+    results = sp.user_playlist_add_tracks(username, playlist_uri, song_uris)
+
+# Takes a username and a term (short, medium, long) in a string
+# Creates a new playlist for the user of their top songs for that term
+def create_playlist_top_songs_by_term(username, term, n=100):
+    playlist = create_playlist(username, term + '_top_songs')
+    songs = list_top_songs_by_term(username, term, n)
+    add_songs_to_playlist(username, playlist, songs)
+
+# Takes a username and a term (short, medium, long) in a string
+# Creates a new playlist for the user of their top artists' songs for that term
+def create_playlist_top_artists_by_term(username, term, n=20, m=5):
+    playlist = create_playlist(username, term+ '_artist_top_songs')
+    artists = list_top_artists_by_term(username, term, 20)
+    songs = []
+    for artist in artists:
+        songs += list_top_songs_by_artist(artist, 5)
+    add_songs_to_playlist(username, playlist, songs)
+
+def create_playlists_top_songs(username, n=100, m=20, l=5):
+    for term in ['short_term', 'medium_term', 'long_term']:
+        create_playlist_top_songs_by_term(username, term, n)
+        create_playlist_top_artists_by_term(username, term, m, l)
+
+def list_new_favorites(username):
+    result = []
+    new = list_top_songs_by_term(username, 'short_term', 100)
+    mid = list_top_songs_by_term(username, 'medium_term', 100)
+    old = list_top_songs_by_term(username, 'long_term', 100)
+    for song in new:
+        if ((song not in mid) and (song not in old)):
+            result += [song]
+    return result
+
+def create_playlist_new_favorites(username):
+    playlist = create_playlist(username, 'new_favorites')
+    songs = list_new_favorites(username)
+    add_songs_to_playlist(username, playlist, songs)
+
+def list_old_favorites(username):
+    result = []
+    new = list_top_songs_by_term(username, 'short_term', 100)
+    mid = list_top_songs_by_term(username, 'medium_term', 100)
+    old = list_top_songs_by_term(username, 'long_term', 100)
+    for song in old:
+        if ((song not in mid) and (song not in new)):
+            result += [song]
+    return result
+
+def create_playlist_old_favorites(username):
+    playlist = create_playlist(username, 'old_favorites')
+    songs = list_old_favorites(username)
+    add_songs_to_playlist(username, playlist, songs)
+
+def list_alltime_favorites(username):
+    result = []
+    new = list_top_songs_by_term(username, 'short_term', 100)
+    mid = list_top_songs_by_term(username, 'medium_term', 100)
+    old = list_top_songs_by_term(username, 'long_term', 100)
+    for song in old:
+        if ((song in new) or (song in mid)):
+            result += [song]
+    return result
+
+def create_playlist_alltime_favorites(username):
+    playlist = create_playlist(username, 'all_time_favorites')
+    songs = list_alltime_favorites(username)
+    add_songs_to_playlist(username, playlist, songs)
+
+
+def list_alltime_favorite_artists(username, n=20):
+    result = []
+    new = list_top_artists_by_term(username, 'short_term', n)
+    mid = list_top_artists_by_term(username, 'medium_term', n)
+    old = list_top_artists_by_term(username, 'long_term', n)
+    for artist in old:
+        if ((artist in new) or (artist in mid)):
+            result += [artist]
+    return result
+
+def list_alltime_favorite_artists_songs(username, n=20):
+    result = []
+    artists = list_alltime_favorite_artists(username)
+    for artist in artists:
+        result += list_top_songs_by_artist(artist, n)
+    return result
+
+def create_playlist_alltime_favorite_artists(username, n=10):
+    playlist = create_playlist(username, 'all_time_favorite_artists')
+    songs = list_alltime_favorite_artists_songs(username, n)
+    add_songs_to_playlist(username, playlist, songs)
